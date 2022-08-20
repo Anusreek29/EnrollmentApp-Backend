648defd8634cd03129b34bd750224a074d722d75
Anusreek29 <102649723+Anusreek29@users.noreply.github.com>
2022-08-21 02:10:00 +0530

    images

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public')); 
+app.use(express.static(path.join(__dirname, "/public")));
 
 // api creation - route definitions
 app.get('/', (req, res) => {
